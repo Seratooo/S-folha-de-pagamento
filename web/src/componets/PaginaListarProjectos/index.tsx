f5b6369903ac2ca5dbd42cab298e3a33caf3caaa
Seratooo <pauloserato2016@gmail.com>
2021-05-25 17:29:44 +0100

    Pag listar projectos

--- web/src/componets/PaginaListarProjectos/index.tsx
+++ web/src/componets/PaginaListarProjectos/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Projecto from '../03_Projecto'
 import './style.css'
+import {Link} from 'react-router-dom'
 export default function PaginaListarProjectos() {
+  
   return(
-    <div>
-      <h1>Pagina Listar Projectos</h1>
-    </div>
+    <>
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}> <Projecto nomeDoProjecto="Projecto Filadelfia"/></Link> 
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}>  <Projecto nomeDoProjecto="Projecto Kiluamge"/> </Link> 
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}>  <Projecto nomeDoProjecto="Projecto Manguiz"/></Link> 
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}>  <Projecto nomeDoProjecto="Projecto Mendelheve"/></Link> 
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}>  <Projecto nomeDoProjecto="Projecto Keve"/></Link> 
+   <Link to="/show-workers/project" style={{textDecoration:'none'}}> <Projecto nomeDoProjecto="Projecto Angola On"/></Link> 
+    </>
   )
 }
