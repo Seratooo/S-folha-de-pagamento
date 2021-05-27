ba17aa16371677abfc9b34b861bdb9c0f578479c
Seratooo <pauloserato2016@gmail.com>
2021-05-27 02:27:16 +0100

    mudei o parametro

--- web/src/components/03_Projecto/index.tsx
+++ web/src/components/03_Projecto/index.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 import Meusclientes from '../../assets/clientes.svg'
 import { dadosProjecto } from '../PaginaProjecto'
 
+interface interfaceProjecto{
+  id: number;
+  name: string;
+  client: string;
+  project_cost: string;
+  date_start: string;
+  date_end: string;
+  completion_percentage:number;
+}
 interface dados{
-  nomeDoProjecto: string;
+  nomeDoProjecto: interfaceProjecto;
 }
 
 export default function Projecto(props:dados) {
@@ -16,7 +25,7 @@ export default function Projecto(props:dados) {
     
         <div className="Principal" style={{background:"#3EE9C0"}} onClick={handleProject}>
         <div className="title" style={{width:'20rem', height:'4rem'}}>
-            <p>{props.nomeDoProjecto}</p>
+            <p key={props.nomeDoProjecto.id}>{props.nomeDoProjecto.name}</p>
         </div>
         <div className="qntd" style={{width:'20rem', height:'7rem'}}>
         <img src={Meusclientes} alt="trabalhadores recentes" style={{width:'6rem'}}/>
