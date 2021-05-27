a108f94f12911e18a50ec5434e5ae3df5a2b429d
Seratooo <pauloserato2016@gmail.com>
2021-05-27 02:23:06 +0100

    add multer / routes (image/lastworker)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,11 @@
 import express, { request, response } from 'express'
+import multer from 'multer'
+import {multerConfig} from './configs/multer'
 import noRelatedWorkerController from './controllers/no-related-workerControler'
 import projectsController from './controllers/projectsController'
 import relatedWorkerController from './controllers/related-workerControler'
 import workerController from './controllers/workerController'
+import path from "path";
 
 const routes = express.Router()
 const worker = new workerController()
@@ -10,16 +13,25 @@ const projects = new projectsController()
 const relatedWork = new relatedWorkerController()
 const noRelatedWork = new noRelatedWorkerController()
 
+routes.post('/image',multer(multerConfig).single("file"),(req,res)=>{
+  console.log(req.file)
+
+  return res.json({hello:'ok'})
+})
+
 routes.get('/workers', worker.getWorkers)
 routes.get('/workers/:id', worker.show)
 routes.get('/workers_related_by_project/:id', worker.showRelatedWorkers_withProject)
 routes.get('/workers_norelated_by_project/:id', worker.showNoRelatedWorkers_withProject)
 
 routes.get('/workers-search', worker.index)
+routes.get('/lastworker', worker.lastWorkers)
+
 
 routes.post('/workers',worker.create)
 
 routes.post('/related-worker',relatedWork.create)
+
 
 routes.post('/no-related-worker',noRelatedWork.create)
 
