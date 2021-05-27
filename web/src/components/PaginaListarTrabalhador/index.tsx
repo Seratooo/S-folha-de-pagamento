97f3c372c7782253a04b8533332a25b8ae27ce5e
Seratooo <pauloserato2016@gmail.com>
2021-05-27 02:28:52 +0100

    to list worker

--- web/src/components/PaginaListarTrabalhador/index.tsx
+++ web/src/components/PaginaListarTrabalhador/index.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UmTrabalhador from '../02_Trabalhador'
 import './style.css'
-import workerImg1 from '../../assets/avatars/worker1.svg'
-import workerImg2 from '../../assets/avatars/worker2.svg'
-import workerImg3 from '../../assets/avatars/worker3.svg'
 import {Link} from 'react-router-dom'
-
+import api from '../../services/api'
+interface dadosTrabalhador{
+  id: number;
+  name: string;
+  imgUrl: string;
+}
 export default function PaginaListarTrabalhador() {
+  const [trabalhadores,setTrabalhadores]=useState<dadosTrabalhador[]>([])
+  useEffect(()=>{
+    api.get('workers').then(Response=>{
+        setTrabalhadores(Response.data)
+    })
+  },[])
   return(
     <>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>     <UmTrabalhador imagem={workerImg1} nome="Mariana Morena Margarida" descricao="Especialista em blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>   <UmTrabalhador  imagem={workerImg2} nome="André Mahatham" descricao="Diretor de blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>  <UmTrabalhador  imagem={workerImg3} nome="Shaddam Amargebraham" descricao="Engenheiro de blabla bla bla"/> </Link>
-
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>   <UmTrabalhador imagem={workerImg1} nome="Mariana Morena Margarida" descricao="Especialista em blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>  <UmTrabalhador imagem={workerImg2} nome="André Mahatham" descricao="Diretor de blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>  <UmTrabalhador imagem={workerImg3} nome="Shaddam Amargebraham" descricao="Engenheiro de blabla bla bla"/> </Link>
-
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>   <UmTrabalhador imagem={workerImg1} nome="Mariana Morena Margarida" descricao="Especialista em blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>   <UmTrabalhador imagem={workerImg2} nome="André Mahatham" descricao="Diretor de blabla bla bla"/> </Link>
-    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>   <UmTrabalhador imagem={workerImg3} nome="Shaddam Amargebraham" descricao="Engenheiro de blabla bla bla"/> </Link>
-
+    {trabalhadores.map(trabalhador=>(
+    <Link to="/show-workers/worker" style={{textDecoration:'none'}}>     <UmTrabalhador imagem={trabalhador.imgUrl} nome={trabalhador.name} descricao={"codigo: " +trabalhador.id} key={trabalhador.id}/> </Link>
+    ))}
     </>
   )
 }
