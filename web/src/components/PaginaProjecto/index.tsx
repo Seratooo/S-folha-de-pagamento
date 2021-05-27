857229860a5ac291f5cffa144e62726e27d61e64
Seratooo <pauloserato2016@gmail.com>
2021-05-27 02:29:45 +0100

    to show data projects

--- web/src/components/PaginaProjecto/index.tsx
+++ web/src/components/PaginaProjecto/index.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import api from '../../services/api';
 
+interface interfaceProjecto{
+  id: number;
+  name: string;
+  client: string;
+  project_cost: string;
+  date_start: string;
+  date_end: string;
+  completion_percentage:number;
+}
 interface dados{
-  nomeDoProjecto: string | '';
+  nomeDoProjecto: interfaceProjecto;
 }
 let data: dados;
 export function dadosProjecto(dado:dados) {
   data = dado
 }
+interface relatedWorkData{
+    name: string;
+}
 
 export default function PaginaProjecto(){
+  const [relatedWorkers,setRelatedWorkers] = useState<relatedWorkData[]>([])
+  
+  const [norelatedWorkers,setNoRelatedWorkers] = useState<relatedWorkData[]>([])
+  
+  useEffect(()=>{
+    api.get(`workers_related_by_project/${data.nomeDoProjecto.id}`).then(Response=>{
+      setRelatedWorkers(Response.data)
+    })
+  },[])
+
+  useEffect(()=>{
+    api.get(`workers_norelated_by_project/${data.nomeDoProjecto.id}`).then(Response=>{
+      setNoRelatedWorkers(Response.data)
+    })
+  },[])
+
     return(
       <div className="principalProjecto">
          <Link to="/show-projects">
@@ -21,20 +50,25 @@ export default function PaginaProjecto(){
       
       <div className="contentProjecto">
       
-      <h1>{data.nomeDoProjecto}</h1>
+      <h1>{data.nomeDoProjecto.name}</h1>
       <div className="Asides">
       <aside className="leftProject">
         <p><strong>Dados do Projecto</strong></p>
-        <p>Projecto isso aquilo</p>
-        <p>blabla projecto</p>
-        <p>faz isso we e bla</p>
-        <p>Intencao wee e wololo</p>
+        <p>Cliente: {data.nomeDoProjecto.client}</p>
+        <p>Custo: {data.nomeDoProjecto.project_cost}</p>
+        <p>Data de Início:{data.nomeDoProjecto.date_start}</p>
+        <p>Data de Término: {data.nomeDoProjecto.date_end}</p>
+        <p><strong>Estado: {data.nomeDoProjecto.completion_percentage}%</strong></p>
       </aside>
       <aside className="rightProjecto">
-        <p><strong>Trabalhadores</strong></p>
-        <p>Fulado de tal</p>
-        <p>Fulana de Tal</p>
-        <p>Minguito Mosquito</p>
+        <p><strong>Trabalhadores Relacionados</strong></p>
+        {relatedWorkers.map(workers=>(
+          <p>{workers.name}</p>
+        ) )}
+        <p><strong>Trabalhadores Não Relacionados</strong></p>
+        {norelatedWorkers.map(workers=>(
+          <p>{workers.name}</p>
+        ) )}
       </aside>
       </div>
       <button>Atualizar dados do projecto</button>
