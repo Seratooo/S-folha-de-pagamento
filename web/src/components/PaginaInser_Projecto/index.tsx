3a1b7757e9882740912db8094ff1029c92254a48
Seratooo <pauloserato2016@gmail.com>
2021-06-05 02:00:48 +0100

    funcoes para add projecto

--- web/src/components/PaginaInser_Projecto/index.tsx
+++ web/src/components/PaginaInser_Projecto/index.tsx
@@ -1,22 +1,62 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import api from '../../services/api';
 import './style.css'
 export default function PaginaProjecto() {
+
+  const [completion_percentage,setCompletion_percentage] = useState('50');
+  const [name,setName] = useState('');
+  const [client,setClient] = useState('');
+  const [project_cost,setProject_cost] = useState('');
+  const [date_start,setDate_start] = useState('');
+  const [date_end,setDate_end]= useState('');
+  function handleCompletetionPercent(event: ChangeEvent<HTMLInputElement>) {
+    setCompletion_percentage(event.target.value);
+  }
+  function handleName(event: ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value)
+  }
+  function handleClient(event:ChangeEvent<HTMLInputElement>) {      
+    setClient(event.target.value)
+  }
+  function handleProjectCust(event:ChangeEvent<HTMLInputElement>) {   
+    setProject_cost(event.target.value)
+  }
+  function handleDataStart(event:ChangeEvent<HTMLInputElement>) { 
+    setDate_start(event.target.value)
+  }
+  function handleDateEnd(event:ChangeEvent<HTMLInputElement>) {
+    setDate_end(event.target.value)
+  }
+
+  function submit() {
+    const data = {
+      name,
+      client,
+      project_cost,
+      date_start,
+      date_end,
+      completion_percentage
+    }
+    
+    api.post('projects',data)
+  }
+
   return(
-    <form action="">
+    <form onSubmit={submit}>
       <fieldset id="fild11">
-        <input type="text" placeholder="Nome do projecto"/>
-        <input type="text" placeholder="Cliente"/>
+        <input type="text" placeholder="Nome do projecto" onChange={handleName}/>
+        <input type="text" placeholder="Cliente" onChange={handleClient}/>
       </fieldset>
 
       <fieldset id="fild22">
-        <input type="text" placeholder="Custo do projecto (Akz)"/>
+        <input type="text" placeholder="Custo do projecto (Akz)" onChange={handleProjectCust}/>
       </fieldset>
       <fieldset id="fild33">
-        <input type="date" name="dataStart" id="dataStart" />
-        <input type="date" name="dataEnd" id="dataEnd" />
+        <input type="date" name="dataStart" id="dataStart" onChange={handleDataStart} />
+        <input type="date" name="dataEnd" id="dataEnd" onChange={handleDateEnd} />
       </fieldset>
-      <fieldset id="fild44">
-        <input type="range" name="Range" id="Range" /> 
+      <fieldset id="fild44" style={{justifyContent:'left',alignItems:'center'}}>
+        <input type="range" name="Range" id="Range" onChange={handleCompletetionPercent} /><p>{completion_percentage}%</p> 
       </fieldset>
       <button>Inserir Projecto</button>
     </form>
