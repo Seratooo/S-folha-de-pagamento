0ef440541a7afb957232216de88a1d96a36e4582
Seratooo <pauloserato2016@gmail.com>
2021-06-05 01:59:44 +0100

    alter type of data / use api / set data

--- web/src/components/02_Trabalhador/index.tsx
+++ web/src/components/02_Trabalhador/index.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api';
 import { dadosTrabalhador } from '../PaginaTrabalhador'
 
 import './style.css'
 
 interface dados{
-  imagem: any;
-  nome: string;
-  descricao: string;
+  id: number;
+  name: string;
+  imgUrl: string;
 }
-
+interface workerData{
+  projectFunc: string;
+}
 
 export default function UmTrabalhador(props: dados){
   function clickiNoTrabalhador() {
     dadosTrabalhador(props);
   }
 
+  const [trabalhador,setTrabalhador]=useState<workerData[]>([]);
+  useEffect(()=>{
+    api.get(`workers/${props.id}`).then(Response=>{
+      setTrabalhador(Response.data)
+      console.log(Response.data)
+    })
+  },[])
+
   return (
     <div className="contentMain" onClick={clickiNoTrabalhador}>
         <div className="contentImg">
-              <img src={props.imagem} alt="Trabalhador 1" />
+              <img src={props.imgUrl} alt="Trabalhador 1" />
         </div>
         <div className="contentText">
-          <h3>{props.nome}</h3>
-          <p>{props.descricao}</p>
+          <h3>{props.name}</h3>
+          <p>{trabalhador.map(tb=>tb.projectFunc)}</p>
         </div>
     </div>
   )
