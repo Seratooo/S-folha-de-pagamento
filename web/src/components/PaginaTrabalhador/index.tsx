ba511c87135f281eddaa74cdb2f36dab0feff0ce
Seratooo <pauloserato2016@gmail.com>
2021-06-06 11:07:27 +0100

    types of data

--- web/src/components/PaginaTrabalhador/index.tsx
+++ web/src/components/PaginaTrabalhador/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import './style.css'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
@@ -10,6 +10,7 @@ interface dados{
   imgUrl: string;
 }
 interface workerData{
+  fk_worker:number;
   level: string;
   project: string;
   projectFunc: string;
@@ -19,8 +20,8 @@ interface workerData{
   imgUrl: string
   responsibility?: string;
   departament?: string;
-  qnt_delays?:number;
-  qnt_houres_worked?:number;
+  qnt_delays?:number | 0;
+  qnt_houres_worked?:number | 0;
 }
 let data:dados;
 
@@ -30,12 +31,51 @@ export function dadosTrabalhador(dado:dados) {
 
 export default function PaginaTrabalhador() {
   const [trabalhador,setTrabalhador]=useState<workerData[]>([]);
+  const [tasks_performed,setTasks_performed] = useState<number>(0)
+  const [task_value,setTask_value] = useState<number>(0)
+  
+  const [qnt_delays,setQnt_delays]= useState<number>(0)
+  const [qnt_houres_worked,setQnt_houres_worked]= useState<number>(0)
+
   useEffect(()=>{
     api.get(`workers/${data.id}`).then(Response=>{
       setTrabalhador(Response.data)
     })
   },[])
 
+  function handleTakPerformed(event:ChangeEvent<HTMLInputElement>) {
+    setTasks_performed(Number(event.target.value))
+  }
+  function handleTaskValue(event:ChangeEvent<HTMLInputElement>) {
+    setTask_value(Number(event.target.value))
+  }
+  function handleQntDelay(event:ChangeEvent<HTMLInputElement>) {
+    setQnt_delays(Number(event.target.value))
+  }
+  function handleQntHoursWorked(event:ChangeEvent<HTMLInputElement>) {
+    setQnt_houres_worked(Number(event.target.value))
+    
+  }
+  async function updateRelated() {
+    const fk_worker = trabalhador.map(tb=>tb.fk_worker)
+    const data = {
+      tasks_performed:trabalhador.map(tb=>tb.tasks_performed + Number(tasks_performed)),
+      task_value: trabalhador.map(tb=>task_value!=0?task_value:tb.task_value),
+    }
+
+    await api.put(`updateRelated/${fk_worker}`,data)
+  }
+  async function updateNoRelated() {
+    const fk_worker = trabalhador.map(tb=>tb.fk_worker)
+    const data = {
+      tasks_performed:trabalhador.map(tb=>tb.tasks_performed + Number(tasks_performed)),
+      task_value: trabalhador.map(tb=>task_value!=0?task_value:tb.task_value),
+      qnt_delays:trabalhador.map(tb=> Number(tb.qnt_delays) + qnt_delays),
+      qnt_houres_worked: trabalhador.map(tb=> Number(tb.qnt_houres_worked) + qnt_houres_worked)
+    }
+    await api.put(`updateNoRelated/${fk_worker}`,data)
+
+  }
   return(
     <div className="PrincipalTrabalhador">
   <Link to="/show-workers"><div className="voltar">
@@ -58,16 +98,16 @@ export default function PaginaTrabalhador() {
               <p><strong>Nível: </strong> {trabalhador.map(tb=>tb.level)}</p>
               <p><strong>Habilidade: </strong> {trabalhador.map(tb=>tb.projectFunc)}</p>
               <p><strong>Projecto Inserido: </strong> {trabalhador.map(tb=>tb.project)}</p>
-              <p><strong>Nº de Tarefas Realizadas: </strong> {trabalhador.map(tb=>tb.tasks_performed)}</p>
-              <p><strong>Complacência: </strong> {trabalhador.map(tb=>tb.task_value)}%</p>
+              <p><strong>Nº de Tarefas Realizadas: </strong> {trabalhador.map(tb=>tb.tasks_performed + Number(tasks_performed))}</p>
+              <p><strong>Complacência: </strong> {trabalhador.map(tb=>task_value!=0?task_value:tb.task_value)}%</p>
               
               {trabalhador.map(
                 tb=>tb.responsibility!=null?
                 <>
                 <p><strong>Responsabilidade: </strong>{tb.responsibility}</p> 
                 <p><strong>Departamento: </strong>{tb.departament}</p> 
-                <p><strong>Nº de Atrasos: </strong>{tb.qnt_delays}</p>
-                <p><strong>Horas trabalhadas: </strong>{tb.qnt_houres_worked}h</p> 
+                <p><strong>Nº de Atrasos: </strong>{Number(tb.qnt_delays) + qnt_delays}</p>
+                <p><strong>Horas trabalhadas: </strong>{Number(tb.qnt_houres_worked) + qnt_houres_worked}h</p> 
                 </>
                 :
                 ' '
@@ -76,25 +116,32 @@ export default function PaginaTrabalhador() {
 
               <fieldset>
 
-              <input type="number" name="" id="number" placeholder="Adicionar Tarefas completadas"/>
+              <input type="number" name="" id="number" placeholder="Adicionar Tarefas completadas" onChange={handleTakPerformed}/>
+    
               {trabalhador.map(
                 tb=>tb.responsibility!=null?
                 <>
-                <input type="number" name="" id="" placeholder="Horas trabalhadas" max="24"/>
-                <input type="number" name="" id="" placeholder="Horas atrasadas" max="24"/>
+                <input type="number" name="" id="" placeholder="Horas trabalhadas" max="24" onChange={handleQntHoursWorked}/>
+                <input type="number" name="" id="" placeholder="Horas atrasadas" max="24" onChange={handleQntDelay}/>
                 </>
                 :
                 ''
               )}
               <div style={{display:'flex',alignItems:'center'}}>
-              <input type="range" name="task_value" id="" placeholder="Complacêcia"/>
-              <p style={{fontFamily:'Roboto',padding:'10px', color:'#353A40'}}>50%</p>
+              <input type="range" name="task_value" id="" placeholder="Complacêcia" value={task_value} onChange={handleTaskValue}/>
+              <p style={{fontFamily:'Roboto',padding:'10px', color:'#353A40'}}>+{task_value}%</p>
               </div>
               </fieldset>
             </div>
             
     </div>
-    <button>Atualizar Dados</button>
+    {trabalhador.map(
+                tb=>tb.responsibility!=null?
+                <button onClick={updateNoRelated}>Atualizar Dados</button>
+                
+                :
+                <button onClick={updateRelated}>Atualizar Dados</button>
+                )}
     </div>
   )
 }
