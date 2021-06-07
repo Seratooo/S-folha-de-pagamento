ab3e4cb6286854be7d88455bd13532e416c0d4f4
Seratooo <pauloserato2016@gmail.com>
2021-06-07 06:04:51 +0100

    news proprierties

--- web/index.js
+++ web/index.js
@@ -1,12 +1,16 @@
 const {app, BrowserWindow} = require("electron")
 function createWindow(){
   const win = new BrowserWindow({
-    minWidth:960,
-    width: 1000, 
+    minWidth:1100,
+    width: 1100, 
     height: 600,
+    webPreferences: {devTools: false},
   })
-  win.loadURL("http://localhost:3000")
+  win.loadURL("http://localhost:3000"),
+  win.removeMenu()
 }
 
 app.on("ready",createWindow)
+
+
 //app.whenReady().then(createWindow)
