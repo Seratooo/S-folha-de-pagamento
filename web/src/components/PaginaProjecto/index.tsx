8e0a13568b0ffd88cc35a28f1f8172d78d742280
Seratooo <pauloserato2016@gmail.com>
2021-06-08 20:52:56 +0100

    localstorage data projecto

--- web/src/components/PaginaProjecto/index.tsx
+++ web/src/components/PaginaProjecto/index.tsx
@@ -3,6 +3,7 @@ import './style.css'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
 import api from '../../services/api';
+import accaoRealizada from '../../assets/accaoRealizada.svg'
 
 interface interfaceProjecto{
   id: number;
@@ -16,29 +17,41 @@ interface interfaceProjecto{
 interface dados{
   nomeDoProjecto: interfaceProjecto;
 }
-let data: dados;
+
 export function dadosProjecto(dado:dados) {
-  data = dado
+  localStorage.setItem("id", ""+dado.nomeDoProjecto.id)
+  localStorage.setItem("name", dado.nomeDoProjecto.name)
+  localStorage.setItem("completion_percentage", ""+dado.nomeDoProjecto.completion_percentage)
+  localStorage.setItem("client", dado.nomeDoProjecto.client)
+  localStorage.setItem("project_cost", dado.nomeDoProjecto.project_cost)
+  
+  localStorage.setItem("date_start", dado.nomeDoProjecto.date_start)
+  localStorage.setItem("date_end", dado.nomeDoProjecto.date_end)
 }
 interface relatedWorkData{
     name: string;
 }
 
 export default function PaginaProjecto(){
+
+  function handleSucess() {
+    document.querySelector('.MessageBackground')?.classList.toggle('off')
+  }
+
   const [relatedWorkers,setRelatedWorkers] = useState<relatedWorkData[]>([])
   const [norelatedWorkers,setNoRelatedWorkers] = useState<relatedWorkData[]>([])
   useEffect(()=>{
-    api.get(`workers_related_by_project/${data.nomeDoProjecto.id}`).then(Response=>{
+    api.get(`workers_related_by_project/${Number(localStorage.getItem("id"))}`).then(Response=>{
       setRelatedWorkers(Response.data)
     })
   },[])
 
   useEffect(()=>{
-    api.get(`workers_norelated_by_project/${data.nomeDoProjecto.id}`).then(Response=>{
+    api.get(`workers_norelated_by_project/${Number(localStorage.getItem("id"))}`).then(Response=>{
       setNoRelatedWorkers(Response.data)
     })
   },[])
-  const [completion_percentage,setCompletion_percentage] = useState(data.nomeDoProjecto.completion_percentage)  
+  const [completion_percentage,setCompletion_percentage] = useState(Number(localStorage.getItem("completion_percentage")))  
   function handleCompletetion(event:ChangeEvent<HTMLInputElement>) {
     setCompletion_percentage(Number(event.target.value))
   }
@@ -46,11 +59,15 @@ export default function PaginaProjecto(){
     const dados = {
       completion_percentage
     }
-    const id = data.nomeDoProjecto.id
+    const id = Number(localStorage.getItem("id"))
     await api.put(`updateProjects/${id}`,dados)
+   
+    document.querySelector('.MessageBackground')?.classList.toggle('off')
     
   }
     return(
+      <>
+
       <div className="principalProjecto">
          <Link to="/show-projects">
           <div className="voltar">
@@ -59,14 +76,14 @@ export default function PaginaProjecto(){
       
       <div className="contentProjecto">
       
-      <h1>{data.nomeDoProjecto.name}</h1>
+      <h1>{localStorage.getItem("name")}</h1>
       <div className="Asides">
       <aside className="leftProject">
         <p><strong>Dados do Projecto</strong></p>
-        <p>Cliente: {data.nomeDoProjecto.client}</p>
-        <p>Custo: {data.nomeDoProjecto.project_cost}</p>
-        <p>Data de Início:{data.nomeDoProjecto.date_start}</p>
-        <p>Data de Término: {data.nomeDoProjecto.date_end}</p>
+        <p>Cliente: {localStorage.getItem("client")}</p>
+        <p>Custo: {localStorage.getItem("project_cost")}</p>
+        <p>Data de Início:{localStorage.getItem("date_start")}</p>
+        <p>Data de Término: {localStorage.getItem("date_end")}</p>
         <p><strong>Estado: {completion_percentage}%</strong></p>
         <fieldset>
         <input type="range" name="task_value" id="" placeholder="Complacêcia" value={completion_percentage} onChange={handleCompletetion}/>
@@ -86,5 +103,20 @@ export default function PaginaProjecto(){
       <button onClick={handleUpdateProject}>Atualizar dados do projecto</button>
       </div>
       </div>
+
+      <div className="MessageBackground off" onClick={handleSucess}>
+            <div className="ShowMessage">
+              <div className="MessageImage">
+                <img src={accaoRealizada} alt="Imagem de conclusão" />
+              </div>
+              <div className="MessageConteudo">
+                <h2>Concluído</h2>
+                <p>Os dados do projecto foram atualizados!!!</p>
+                <button>OK</button>
+              </div>
+            </div>
+        </div>
+
+      </>
     )
 }
