367300443d5b9708ac151b103f4f71137c6d18a4
Seratooo <pauloserato2016@gmail.com>
2021-06-09 14:25:44 +0100

    organizando as routas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,7 +5,6 @@ import noRelatedWorkerController from './controllers/no-related-workerControler'
 import projectsController from './controllers/projectsController'
 import relatedWorkerController from './controllers/related-workerControler'
 import workerController from './controllers/workerController'
-import path from "path";
 
 const routes = express.Router()
 const worker = new workerController()
@@ -13,40 +12,31 @@ const projects = new projectsController()
 const relatedWork = new relatedWorkerController()
 const noRelatedWork = new noRelatedWorkerController()
 
-routes.post('/image',multer(multerConfig).single("file"),(req,res)=>{
-  console.log(req.file)
-
-  return res.json({hello:'ok'})
-})
 
 routes.get('/workers', worker.getWorkers)
 routes.get('/workers/:id', worker.show)
 routes.get('/allRelatedworkers/', worker.showallRelated)
 routes.get('/allNoRelatedworkers/', worker.showallNoRelated)
-
-
-
 routes.get('/workers_related_by_project/:id', worker.showRelatedWorkers_withProject)
 routes.get('/workers_norelated_by_project/:id', worker.showNoRelatedWorkers_withProject)
-
 routes.get('/workers-search', worker.index)
 routes.get('/lastworker', worker.lastWorkers)
+routes.get('/bestworker', worker.bestWorker)
+routes.get('/countRelated',worker.countlRelated)
+routes.get('/countNoRelated',worker.countlNoRelated)
 
+routes.get('/projects',projects.getAllProjects)
+routes.get('/countprojects',projects.countAllProjects)
+routes.get('/mediaprojects',projects.ProjectsCompleted)
 
 routes.post('/workers',worker.create)
-
 routes.post('/related-worker',relatedWork.create)
-
-
 routes.post('/no-related-worker',noRelatedWork.create)
-
 routes.post('/projects',projects.create)
-routes.get('/projects',projects.getAllProjects)
+routes.post('/image',multer(multerConfig).single("file"),(req,res)=>{return res.json({message:'ok'})})
 
 routes.put('/updateRelated/:id',relatedWork.update)
 routes.put('/updateNoRelated/:id',noRelatedWork.update)
 routes.put('/updateProjects/:id',projects.update)
-
-
 
 export default routes
