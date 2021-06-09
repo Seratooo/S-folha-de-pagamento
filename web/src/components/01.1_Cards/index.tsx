bff3b1025a93bf6b540d0726359faff6d6978e5b
Seratooo <pauloserato2016@gmail.com>
2021-06-09 14:28:30 +0100

    carregando dados da bd

--- web/src/components/01.1_Cards/index.tsx
+++ web/src/components/01.1_Cards/index.tsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
 import {Start,Start2,Start3,Start4,Start5} from '../01.11_StisticComponents/statisticData'
+import {Link} from 'react-router-dom'
+
+interface worker{
+  qntd: number;
+}
+interface projects{
+  qntdProjectos:number
+}
+interface media{
+  media:number;
+}
+interface best{
+  name:string;
+}
 
 export default function PaginaInicial(){
+  const [qntRelated,setQntRelated] = useState<worker[]>([{qntd:0}]);
+  useEffect(()=>{
+    api.get('countRelated').then(Response=>{
+      setQntRelated(Response.data)
+    })
+  })
+
+  const [qntNoRelated,setNoQntRelated] = useState<worker[]>([{qntd:0}]);
+  useEffect(()=>{
+    api.get('countNoRelated').then(Response=>{
+      setNoQntRelated(Response.data)
+    })
+  })
+
+  const [qntProjects,setProjects] = useState<projects[]>([{qntdProjectos:0}]);
+  useEffect(()=>{
+    api.get('countprojects').then(Response=>{
+      setProjects(Response.data)
+    })
+  })
+
+  const [media,setMedia] = useState<media[]>([{media:0}]);
+  useEffect(()=>{
+    api.get('mediaprojects').then(Response=>{
+      setMedia(Response.data)
+    })
+  })
+
+  const [best,setBest] = useState<best[]>([{name:''}]);
+  useEffect(()=>{
+    api.get('bestworker').then(Response=>{
+      setBest(Response.data)
+    })
+  })
   return(
     <>
-              <Start title="Trabalhadores Vinculados" qnt="1.224"/>
-              <Start title="Trabalhadores Não Vinculados" qnt="1.524"/>
-              <Start title="Projectos" qnt="524"/>
-              <Start2/>
-              <Start3/>
+              <Start title="Trabalhadores Vinculados" qnt={""+qntRelated.map(qR=>qR.qntd)}/>
+              <Start title="Trabalhadores Não Vinculados" qnt={""+qntNoRelated.map(qNR=>qNR.qntd)}/>
+              <Start title="Projectos" qnt={""+qntProjects.map(qntP=>qntP.qntdProjectos)}/>
+              <Start2 media={Number(media.map(m=>m.media))}/>
+              <Start3 melhorTrabalhador={String(best.map(n=>n.name))}/>
               <Start4/>
               <Start5/>
               
