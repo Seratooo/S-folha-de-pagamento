1b1eb8bf9d20155a7e273790a1e95c9a84dd6580
Seratooo <pauloserato2016@gmail.com>
2021-06-09 14:28:03 +0100

    modify cards

--- web/src/components/01.11_StisticComponents/statisticData.tsx
+++ web/src/components/01.11_StisticComponents/statisticData.tsx
@@ -8,6 +8,12 @@ interface data{
   title: string;
   qnt: string;
 }
+interface data2{
+  media: number;
+}
+interface data3{
+  melhorTrabalhador: string;
+}
 export const  Start = (props:data) =>{
   return(
     <div className="Principal">
@@ -21,26 +27,26 @@ export const  Start = (props:data) =>{
   )
 }
 
-export const Start2 = ()=>{
+export const Start2 = (props:data2)=>{
   return(
     <div className="Principal" style={{background:"#3EE9C0"}}>
     <div className="title" style={{width:'24rem', height:'4rem'}}>
         <p>Projectos Concluídos</p>
     </div>
     <div className="qntd" style={{width:'24rem', height:'6rem'}}>
-        <p>65%</p>
+        <p>{props.media}%</p>
     </div>
     </div>
   )
 }
-export const Start3 = ()=>{
+export const Start3 = (props:data3)=>{
   return(
     <div className="Principal" >
     <div className="title" style={{width:'31rem', height:'4rem'}}>
         <p>Trabalhador com melhor desempenho</p>
     </div>
     <div className="qntd" style={{width:'31rem', height:'6rem'}}>
-        <p>Abubacar Correia</p>
+        <p>{props.melhorTrabalhador}</p>
     </div>
     </div>
   )
@@ -50,7 +56,7 @@ export const Start4 = ()=>{
   return(
     <div className="Principal" >
     <div className="title" style={{width:'31rem', height:'5rem'}}>
-        <p>Trabalhadores recentes</p>
+        <p>Gerencie Trabalhadores</p>
     </div>
     <div className="qntd" style={{width:'31rem', height:'9rem'}}>
         <img src={trabalhadores_recentes} alt="trabalhadores recentes" style={{width:'8rem'}}/>
@@ -63,7 +69,7 @@ export const Start5 = ()=>{
   return(
     <div className="Principal" style={{background:"#3EE9C0"}}>
     <div className="title" style={{width:'24rem', height:'5rem'}}>
-        <p>Projectos Concluídos</p>
+        <p>Gerencie Projectos</p>
     </div>
     <div className="qntd" style={{width:'24rem', height:'9rem'}}>
     <img src={Meusclientes} alt="trabalhadores recentes" style={{width:'8rem'}}/>
