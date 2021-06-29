730a16fc018819dfdecce4e43c030be26c6311f1
Seratooo <pauloserato2016@gmail.com>
2021-06-29 15:26:23 +0100

    Testes

--- web/index.js
+++ web/index.js
@@ -2,8 +2,8 @@ const {app, BrowserWindow} = require("electron")
 function createWindow(){
   const win = new BrowserWindow({
     minWidth:1100,
-    width: 1100, 
-    height: 600,
+    width: 1200, 
+    height: 650,
     webPreferences: {devTools: false},
   })
   win.loadURL("http://localhost:3000"),
